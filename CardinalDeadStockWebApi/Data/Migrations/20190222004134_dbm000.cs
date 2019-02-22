7424b92a6e6cdb09f71b58dd37cc92e678b7c8c6
Ryan Rauch <rauch.ryan@gmail.com>
2019-02-21 19:29:43 -0600

    init

---
+++ CardinalDeadStockWebApi/Data/Migrations/20190222004134_dbm000.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CardinalDeadStockWebApi.Data.Migrations
+{
+    public partial class dbm000 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
